d18bd25a21b8d111b86a61b71319fa33a75a04bc
xofbd <dfox09@gmail.com>
2018-03-10 01:05:45 -0500

    add private attributes

--- dfn.py
+++ dfn.py
@@ -16,44 +16,44 @@ class FractureNetwork(object):
         self.H = np.array(H)
         self.w = np.array(w)
 
-        self.n_nodes = 1 + conn.max()
+        self.n_nodes = 1 + self.conn.max()
 
     def __calculate_conductance(self):
         self.C = fluid.rho * self.w**3 * self.H / (12 * fluid.mu * self.L)
 
     def __assemble_D(self):
         self.calculate_conductance()
-        self.D = np.zeros((self.n_nodes, self.n_nodes))
+        self._D = np.zeros((self.n_nodes, self.n_nodes))
         elemental_D = np.array([[1, -1], [-1, 1]])
 
         for i, seg in enumerate(self.conn):
             D_e = self.C[i] * elemental_D
-            self.D[np.ix_(seg, seg)] += D_e
+            self._D[np.ix_(seg, seg)] += D_e
 
     def __assemble_f(self):
-        self.f = np.zeros(self.n_nodes)
+        self._f = np.zeros(self.n_nodes)
 
         nodes = self.point_sources.keys()
         values = self.point_sources.values()
-        self.f[nodes] = values
+        self._f[nodes] = values
 
     def __apply_EBC(self):
         nodes = self.essential_bc.keys()
         values = self.essential_bc.values()
 
-        self.f -= np.dot(self.D[:, nodes], values)
-        self.f[nodes] = values
+        self._f -= np.dot(self._D[:, nodes], values)
+        self._f[nodes] = values
 
-        self.D[nodes, :] = 0
-        self.D[:, nodes] = 0
-        self.D[nodes, nodes] = 1
+        self._D[nodes, :] = 0
+        self._D[:, nodes] = 0
+        self._D[nodes, nodes] = 1
 
     def solve_pressure(self):
         self.__assemble_D()
         self.__assemble_f()
         self.__apply_EBC()
 
-        self.P = np.linalg.solve(self.D, self.f)
+        self.P = np.linalg.solve(self._D, self._f)
 
     def calculate_flow(self, fluid, essential_bc, point_sources):
         """ Calculate flow throughout the fracture network.
