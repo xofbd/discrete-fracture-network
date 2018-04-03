bec546afe6b94371ea92667bb20d76df91f04a1f
xofbd <dfox09@gmail.com>
2018-04-03 12:52:29 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dfn',
+    packages='dfn',
+    author='Don Bruce Fox',
+    author_email='dfox09@gmail.com',
+    description='An analytical thermohydraulic model for discretely fractured\
+    geothermal reservoirs',
+    install_requires=['networkx', 'numpy']
+    license='MIT'
+)
