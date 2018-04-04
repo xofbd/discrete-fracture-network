b4545113b56dcd331ff8bc854f9dc6a458643a42
xofbd <dfox09@gmail.com>
2018-04-04 11:22:06 -0400

    packagesshould be a list

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dfn',
-    packages='dfn',
+    packages=['dfn'],
     author='Don Bruce Fox',
     author_email='dfox09@gmail.com',
     description='an analytical thermohydraulic model for discretely fractured\
