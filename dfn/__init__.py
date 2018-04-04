60f100f8549b82b44f2c4db6a309b0f21be9f254
xofbd <dfox09@gmail.com>
2018-04-04 13:08:44 -0400

    add FractureNetwork

--- dfn/__init__.py
+++ dfn/__init__.py
@@ -1,3 +1,4 @@
 from dfn.Fluid import Fluid
+from dfn.FractureNetwork import FractureNetwork
 from dfn.FractureNetworkFlow import FractureNetworkFlow
 from dfn.FractureNetworkThermal import FractureNetworkThermal
