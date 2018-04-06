bdec8dc74a3e44ce30f6e0c0f2be31d411cad1b8
xofbd <dfox09@gmail.com>
2018-04-05 20:15:37 -0400

    add docstring

--- dfn/tests/test_fluids.py
+++ dfn/tests/test_fluids.py
@@ -17,17 +17,20 @@ class TestFluid(unittest.TestCase):
                              heat_capacity=self.c_f)
 
     def test_density_attr(self):
+        """Test density attribute."""
         self.assertEqual(self.rho, self.fluid_1.rho)
 
     def test_viscosity_attr(self):
+        """Test viscosity attribute."""
         self.assertEqual(self.mu, self.fluid_1.mu)
 
     def test_default_value(self):
+        """Test default value for heat capacity."""
         self.assertIsNone(self.fluid_1.c_f)
 
     def test_heat_capacity_attr(self):
+        """Test heat capacity attribute."""
         self.assertEqual(self.c_f, self.fluid_2.c_f)
-
 
 if __name__ == '__main__':
     unittest.main()
