d155d93ec0341f09a59f118709ee96f38b40d5a5
xofbd <dfox09@gmail.com>
2018-04-30 16:40:18 -0400

    update version number

--- dfn/__init__.py
+++ dfn/__init__.py
@@ -3,4 +3,4 @@ from dfn.FractureNetwork import FractureNetwork
 from dfn.FractureNetworkFlow import FractureNetworkFlow
 from dfn.FractureNetworkThermal import FractureNetworkThermal
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dfn',
-    version='1.0.0',
+    version='1.0.1',
     packages=['dfn'],
     author='Don Bruce Fox',
     author_email='dfox09@gmail.com',
