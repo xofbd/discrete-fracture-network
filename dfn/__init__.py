652786aeaa537c83945ab1da9efaa0faa68b9c83
xofbd <dfox09@gmail.com>
2019-11-03 22:28:27 -0500

    bump version to 1.1.1

--- dfn/__init__.py
+++ dfn/__init__.py
@@ -6,4 +6,4 @@ from dfn.FractureNetworkThermal import FractureNetworkThermal
 from dfn.read_json import (read_json, read_flow_json, read_fluid_json,
                            read_network_json, read_thermal_json)
 
-__version__ = '1.1.0'
+__version__ = '1.1.1'
