121fd411c5aad0e7b291fb54e774993a3e47f727
xofbd <dfox09@gmail.com>
2019-11-03 22:28:05 -0500

    add read_json to __init__

--- dfn/__init__.py
+++ dfn/__init__.py
@@ -1,7 +1,9 @@
+from dfn.base import Base
 from dfn.Fluid import Fluid
 from dfn.FractureNetwork import FractureNetwork
 from dfn.FractureNetworkFlow import FractureNetworkFlow
 from dfn.FractureNetworkThermal import FractureNetworkThermal
-from dfn.read_json import read_flow_json, read_network_json, read_thermal_json
+from dfn.read_json import (read_json, read_flow_json, read_fluid_json,
+                           read_network_json, read_thermal_json)
 
 __version__ = '1.1.0'
