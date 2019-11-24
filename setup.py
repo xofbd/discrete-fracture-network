c708e18f155bd2a07482ca32a84f6e92c6b1f7c2
xofbd <dfox09@gmail.com>
2019-11-24 18:29:06 -0500

    rely on single source of truth for package versions

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup
 
+from dfn import __version__
+
+with open('requirements.txt', 'r') as f:
+    requirements = [line.strip() for line in f]
+
+
 setup(
     name='dfn',
-    version='1.1.0',
+    version=__version__,
     packages=['dfn'],
     author='Don Bruce Fox',
     author_email='dfox09@gmail.com',
     description='an analytical thermohydraulic model for discretely fractured\
     geothermal reservoirs',
-    install_requires=['networkx', 'numpy', 'scipy'],
+    install_requires=requirements,
     test_suite='nose.collector',
     test_require=['nose'],
     license='MIT'
