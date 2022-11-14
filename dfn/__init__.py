469de71ca8b8a42c27b00ae963ceefa091066f51
xofbd <dfox09@gmail.com>
2022-11-14 17:16:13 -0500

    Read version from pyproject.toml

--- dfn/__init__.py
+++ dfn/__init__.py
@@ -1,9 +1,22 @@
+import sys
+
 from dfn.base import Base
 from dfn.Fluid import Fluid
 from dfn.FractureNetwork import FractureNetwork
 from dfn.FractureNetworkFlow import FractureNetworkFlow
 from dfn.FractureNetworkThermal import FractureNetworkThermal
-from dfn.read_json import (read_json, read_flow_json, read_fluid_json,
-                           read_network_json, read_thermal_json)
+from dfn.read_json import (
+        read_json,
+        read_flow_json,
+        read_fluid_json,
+        read_network_json,
+        read_thermal_json
+    )
 
-__version__ = '1.1.2'
+
+if sys.version_info >= (3, 8):
+    from importlib import metadata
+else:
+    import importlib_metadata as metadata
+
+__version__ = metadata.version("dfn")
